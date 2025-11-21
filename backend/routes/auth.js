f359da34e3e488593b4693956dc97ac50122f3e2
ChayakornChaiyaphan <6632038021@student.chula.ac.th>
2025-11-21 15:52:06 +0700

    fix config issues

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { register, login, getMe, logout } = require('../controllers/auth');
+const { protect } = require('../middleware/auth');
 
 const router = express.Router();
 router.post('/register', register).post('/login', login);
-router.get('/me', getMe).get('/logout', logout);
+router.route('/me').get(protect, getMe);
+router.route('/logout').get(protect, logout);
 
 module.exports = router;
